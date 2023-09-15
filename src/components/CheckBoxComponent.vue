356acb9c63341f849f752c7d21d2dd635c79e40c
Fabio Carvalho <fabio.carvalllho@live.com>
2023-09-15 15:13:36 -0300

    include the rule for the columns when the check is selected

---
+++ src/components/CheckBoxComponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="checkbox-container" :class="{ 'selected': isSelected }" @click="toggleCheckbox">
+        <input type="checkbox" class="checkbox-input" :checked="isSelected" />
+        <span class="checkbox-label">{{ label }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['label', 'isSelected'],
+    methods: {
+        toggleCheckbox() {
+            this.$emit('checkboxToggled', this.label);
+        }
+    }
+};
+</script>
+
+<style>
+.checkbox-container {
+    border: 2px solid lightgray;
+    border-radius: 5px;
+    padding: 10px;
+    display: inline-block;
+    margin: 5px;
+    cursor: pointer;
+    position: relative;
+}
+
+.checkbox-container.selected {
+    border-color: #5564a4;
+}
+
+.checkbox-input {
+    border: 2px solid lightgray;
+    border-radius: 50%;
+    display: inline-block;
+    cursor: pointer;
+}
+
+.checkbox-input.checked {
+    background-color: #5564a4;
+    border-color: #5564a4;
+}
+
+.checkbox-label {
+    margin-left: 5px;
+}
+
+.checkbox-container.selected::after {
+    content: "";
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-top: 10px solid #5564a4;
+    position: absolute;
+    bottom: -10px;
+    left: 50%;
+    transform: translateX(-50%);
+}
+</style>
--- src/components/MainCardComponent.vue
+++ src/components/MainCardComponent.vue
@@ -1,40 +1,85 @@
 <template>
     <div class="main-card p-4" :style="{ backgroundColor: '#ffffff', opacity: opacityLevel }">
-        <ColumnComponent :title="'LEAD'" :bgColor="'#f5f1ff'" :style="{ borderColor: strongColor }"  :toggleStyle="'flat-style'" :toggleColors="{ toggleBg: '#e5dafc', circleColor: '#d4c1f9' }"/>
-        <ColumnComponent :title="'RFP IN PROGRESS'" :bgColor="'#fbf6f0'" :style="{ borderColor: strongColor }"  :toggleStyle="'flat-style'" :toggleColors="{ toggleBg: '#f4d7b9', circleColor: '#f7e5cf' }"/>
-        <ColumnComponent :title="'SUBMITTED'" :bgColor="'#eaf4fe'" :style="{ borderColor: strongColor }"  :toggleStyle="'flat-style'" :toggleColors="{ toggleBg: '#add0fa', circleColor: '#cae2fc' }"/>
-        <ColumnComponent :title="'WON'" :bgColor="'#e5f4f7'" :style="{ borderColor: strongColor }"  :toggleStyle="'flat-style'" :toggleColors="{ toggleBg: '#a1dce4', circleColor: '#bfeaf1' }"/>
-        <ColumnComponent :title="'LOST'" :bgColor="'#fcf1f5'" :style="{ borderColor: strongColor }"  :toggleStyle="'flat-style'" :toggleColors="{ toggleBg: '#f0c1d1', circleColor: '#f6dbe4' }"/>
-        <ColumnComponent :title="'CLOSED'" :bgColor="'#f3f7fa'" :style="{ borderColor: strongColor }"  :toggleStyle="'flat-style'" :toggleColors="{ toggleBg: '#ced5df', circleColor: '#e4e9ef' }"/>
+        <h2 style="padding-top: 10px;">Please select the types of sales pipeline that best fits to your company</h2>
+        <div class="check-container">
+            <CheckboxComponent :label="'Basic'" :isSelected="selectedCheckbox === 'Basic'" @checkboxToggled="sortColumns" />
+            <CheckboxComponent :label="'Advanced'" :isSelected="selectedCheckbox === 'Advanced'"
+                @checkboxToggled="sortColumns" />
+            <CheckboxComponent :label="'Expert'" :isSelected="selectedCheckbox === 'Expert'"
+                @checkboxToggled="sortColumns" />
+            <CheckboxComponent :label="'Custom'" :isSelected="selectedCheckbox === 'Custom'"
+                @checkboxToggled="sortColumns" />
+        </div>
+        <div class="columns-container">
+            <ColumnComponent v-if="visibleColumns.includes('LEAD')" :title="'LEAD'" :bgColor="'#f5f1ff'"
+                :style="{ borderColor: strongColor }" :toggleStyle="'flat-style'"
+                :toggleColors="{ toggleBg: '#e5dafc', circleColor: '#d4c1f9' }" />
+            <ColumnComponent v-if="visibleColumns.includes('RFP IN PROGRESS')" :title="'RFP IN PROGRESS'"
+                :bgColor="'#fbf6f0'" :style="{ borderColor: strongColor }" :toggleStyle="'flat-style'"
+                :toggleColors="{ toggleBg: '#f4d7b9', circleColor: '#f7e5cf' }" />
+            <ColumnComponent v-if="visibleColumns.includes('SUBMITTED')" :title="'SUBMITTED'" :bgColor="'#eaf4fe'"
+                :style="{ borderColor: strongColor }" :toggleStyle="'flat-style'"
+                :toggleColors="{ toggleBg: '#add0fa', circleColor: '#cae2fc' }" />
+            <ColumnComponent v-if="visibleColumns.includes('WON')" :title="'WON'" :bgColor="'#e5f4f7'"
+                :style="{ borderColor: strongColor }" :toggleStyle="'flat-style'"
+                :toggleColors="{ toggleBg: '#a1dce4', circleColor: '#bfeaf1' }" />
+            <ColumnComponent v-if="visibleColumns.includes('LOST')" :title="'LOST'" :bgColor="'#fcf1f5'"
+                :style="{ borderColor: strongColor }" :toggleStyle="'flat-style'"
+                :toggleColors="{ toggleBg: '#f0c1d1', circleColor: '#f6dbe4' }" />
+            <ColumnComponent v-if="visibleColumns.includes('CLOSED')" :title="'CLOSED'" :bgColor="'#f3f7fa'"
+                :style="{ borderColor: strongColor }" :toggleStyle="'flat-style'"
+                :toggleColors="{ toggleBg: '#ced5df', circleColor: '#e4e9ef' }" />
+        </div>
+
     </div>
 </template>
   
 <script>
 import ColumnComponent from './ColumnComponent.vue';
+import CheckboxComponent from './CheckBoxComponent.vue'
 
 export default {
     props: ['bgColor', 'strongColor', 'toggleStyle'],
     components: {
-        ColumnComponent
+        ColumnComponent,
+        CheckboxComponent
     },
     data() {
         return {
-            opacityLevel: 1
+            selectedCheckbox: null,
+            opacityLevel: 1,
+            visibleColumns: ['LEAD', 'RFP IN PROGRESS', 'SUBMITTED', 'WON', 'LOST', 'CLOSED']
         };
     },
-    mounted() {
-        window.addEventListener('scroll', this.handleScroll);
-    },
-    beforeUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    },
     methods: {
-        handleScroll() {
-            const scrolled = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
-            if (scrolled > 0.6) {
-                this.opacityLevel = 0.8 + 0.2 * (scrolled - 0.6) / 0.4;
+        sortColumns(label) {        
+            if (this.selectedCheckbox === label) {
+                this.selectedCheckbox = null;
+                this.showAllColumns();
             } else {
-                this.opacityLevel = 1;
+                this.selectedCheckbox = label;
+                this.sortBasedOnSelection(label); 
+            }
+        },
+        showAllColumns() {
+            this.visibleColumns = ['LEAD', 'RFP IN PROGRESS', 'SUBMITTED', 'WON', 'LOST', 'CLOSED'];
+        },
+        sortBasedOnSelection(label) {
+            switch (label) {
+                case 'Basic':
+                    this.visibleColumns = ['LEAD', 'RFP IN PROGRESS'];
+                    break;
+                case 'Advanced':
+                    this.visibleColumns = ['LEAD', 'RFP IN PROGRESS', 'SUBMITTED', 'WON'];
+                    break;
+                case 'Expert':
+                    this.visibleColumns = ['LEAD', 'RFP IN PROGRESS', 'SUBMITTED', 'WON', 'LOST'];
+                    break;
+                case 'Custom':
+                    this.visibleColumns = ['LEAD', 'RFP IN PROGRESS', 'SUBMITTED', 'WON', 'LOST', 'CLOSED'];
+                    break;
+                default:
+                    this.visibleColumns = ['LEAD', 'RFP IN PROGRESS', 'SUBMITTED', 'WON', 'LOST', 'CLOSED'];
             }
         }
     }
@@ -43,6 +88,10 @@ export default {
 </script>
   
 <style>
+.boxes {
+    margin-bottom: 1px;
+}
+
 .main-card {
     width: 100%;
     margin-left: 2%;
@@ -53,6 +102,7 @@ export default {
     justify-content: center;
     align-items: center;
     border-radius: 1%;
+    flex-direction: column;
 }
 
 .main-card::after {
@@ -70,12 +120,25 @@ export default {
     border: 1px solid;
 }
 
+.check-container {
+    display: flex;
+    margin-bottom: 5px;
+    justify-content: center;
+}
+
+.columns-container {
+    /* Estilos para o contêiner de colunas */
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
 @media (min-width: 768px) {
-  .main-card {
-    width: 99%;
-    margin-left: auto;
-    margin-right: auto;
-  }
+    .main-card {
+        width: 99%;
+        margin-left: auto;
+        margin-right: auto;
+    }
 }
 </style>
   
